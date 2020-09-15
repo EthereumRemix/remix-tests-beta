1ef084e048cfa4e251233f815e67d590204f95bb
Aniket-Engg <aniketengg.225@gmail.com>
2020-09-15 12:17:15 +0530

    tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     name: 'remix-tests',
-    preset: '../../jest.config.js',
     verbose: true,
     silent: true, // Silent console messages, specially the 'remix-simulator' ones
     transform: {
@@ -9,15 +8,5 @@ module.exports = {
     rootDir: "./",
     testTimeout: 30000,
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html', 'json'],
-    // Coverage
-    collectCoverage: true,
-    coverageReporters: ['text', 'text-summary'],
-    collectCoverageFrom: [
-      "**/*.ts",
-      "!**/sol/**",
-      "!src/types.ts",
-      "!src/logger.ts"
-    ],
-    coverageDirectory: '../../coverage/libs/remix-tests',
   };
   
